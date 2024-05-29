8b4604567c1efb01a91e36aa39be3be1215eea18
Aneudy Consuegra <ilovethe80s90s@yahoo.com>
2024-05-29 00:34:48 -0400

    Was trying to see how barcode works. (it didnt)

--- main.py
+++ main.py
@@ -1,58 +1,92 @@
-
+import asyncio
 import flet as ft
 
 
-def main(page):
+class Countdown(ft.Text):
+    def __init__(self, seconds):
+        super().__init__()
+        self.seconds = seconds
 
-    page.adaptive = True
+    def did_mount(self):
+        self.running = True
+        self.page.run_task(self.update_timer)
 
-    page.appbar = ft.AppBar(
-        leading=ft.TextButton("New", style=ft.ButtonStyle(padding=0)),
-        title=ft.Text("Adaptive AppBar"),
-        actions=[
-            ft.IconButton(ft.cupertino_icons.ADD,
-                          style=ft.ButtonStyle(padding=0))
-        ],
-        bgcolor=ft.colors.with_opacity(
-            0.04, ft.cupertino_colors.SYSTEM_BACKGROUND),
-    )
+    def will_unmount(self):
+        self.running = False
 
-    page.navigation_bar = ft.NavigationBar(
-        destinations=[
-            ft.NavigationDestination(icon=ft.icons.EXPLORE, label="Explore"),
-            ft.NavigationDestination(icon=ft.icons.COMMUTE, label="Commute"),
-            ft.NavigationDestination(
-                icon=ft.icons.BOOKMARK_BORDER,
-                selected_icon=ft.icons.BOOKMARK,
-                label="Bookmark",
-            ),
-        ],
-        border=ft.Border(
-            top=ft.BorderSide(color=ft.cupertino_colors.SYSTEM_GREY2, width=0)
-        ),
-    )
-
-    page.add(
-        ft.SafeArea(
-            ft.Column(
-                [
-                    ft.Checkbox(value=False, label="Dark Mode"),
-                    ft.Text("First field:"),
-                    ft.TextField(keyboard_type=ft.KeyboardType.TEXT),
-                    ft.Text("Second field:"),
-                    ft.TextField(keyboard_type=ft.KeyboardType.TEXT),
-                    ft.Switch(label="A switch"),
-                    ft.FilledButton(content=ft.Text("Adaptive button")),
-                    ft.Text("Text line 1"),
-                    ft.Text("Text line 2"),
-                    ft.Text("Text line 3"),
-                ]
-            )
-        )
-    )
+    async def update_timer(self):
+        while self.seconds and self.running:
+            mins, secs = divmod(self.seconds, 60)
+            self.value = "{:02d}:{:02d}".format(mins, secs)
+            self.update()
+            await asyncio.sleep(1)
+            self.seconds -= 1
 
 
-ft.app(target=main)
+def main(page: ft.Page):
+    page.add(Countdown(120), Countdown(60))
+
+
+ft.app(main)
+
+
+''''''''''''''''''''''''''''''''''''''
+
+
+# import flet as ft
+
+
+# def main(page):
+
+#     page.adaptive = True
+
+#     page.appbar = ft.AppBar(
+#         leading=ft.TextButton("New", style=ft.ButtonStyle(padding=0)),
+#         title=ft.Text("Adaptive AppBar"),
+#         actions=[
+#             ft.IconButton(ft.cupertino_icons.ADD,
+#                           style=ft.ButtonStyle(padding=0))
+#         ],
+#         bgcolor=ft.colors.with_opacity(
+#             0.04, ft.cupertino_colors.SYSTEM_BACKGROUND),
+#     )
+
+#     page.navigation_bar = ft.NavigationBar(
+#         destinations=[
+#             ft.NavigationDestination(icon=ft.icons.EXPLORE, label="Explore"),
+#             ft.NavigationDestination(icon=ft.icons.COMMUTE, label="Commute"),
+#             ft.NavigationDestination(
+#                 icon=ft.icons.BOOKMARK_BORDER,
+#                 selected_icon=ft.icons.BOOKMARK,
+#                 label="Bookmark",
+#             ),
+#         ],
+#         border=ft.Border(
+#             top=ft.BorderSide(color=ft.cupertino_colors.SYSTEM_GREY2, width=0)
+#         ),
+#     )
+
+#     page.add(
+#         ft.SafeArea(
+#             ft.Column(
+#                 [
+#                     ft.Checkbox(value=False, label="Dark Mode"),
+#                     ft.Text("First field:"),
+#                     ft.TextField(keyboard_type=ft.KeyboardType.TEXT),
+#                     ft.Text("Second field:"),
+#                     ft.TextField(keyboard_type=ft.KeyboardType.TEXT),
+#                     ft.Switch(label="A switch"),
+#                     ft.FilledButton(content=ft.Text("Adaptive button")),
+#                     ft.Text("Text line 1"),
+#                     ft.Text("Text line 2"),
+#                     ft.Text("Text line 3"),
+#                 ]
+#             )
+#         )
+#     )
+
+
+# ft.app(target=main)
 
 
 ''''''''''''''''''''''''''''''''''''''''''''
