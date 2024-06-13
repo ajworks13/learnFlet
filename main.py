6f9688bc78050eb3a625f6a38c3e66bb0dc66596
Aneudy Consuegra <ilovethe80s90s@yahoo.com>
2024-06-13 00:11:29 -0400

    event variable not accessed somehow

--- main.py
+++ main.py
@@ -9,17 +9,33 @@ def main(page: Page) -> None:
     def route_change(e: RouteChangeEvent) -> None:
         page.views.clear()
 
-        page.views.append(
-            View(
-                route='/',
-                controls=[
-                    AppBar(title=Text("Home"), bgcolor='blue'),
-                    Text(value='Home', size=30),
-                    ElevatedButton(text='Go to store',
-                                   on_click=lambda _: page.go('/store'))
-                ],
-                vertical_alignment=MainAxisAlignment.CENTER,
-                horizontal_alignment=CrossAxisAlignment.CENTER,
-                spacing=26
+        if page.route == '/store':
+
+            page.views.append(
+                View(
+                    route='/store',
+                    controls=[
+                        AppBar(title=Text("Store"), bgcolor='blue'),
+                        Text(value='Store', size=30),
+                        ElevatedButton(text='Go back',
+                                       on_click=lambda _: page.go('/'))
+                    ],
+                    vertical_alignment=MainAxisAlignment.CENTER,
+                    horizontal_alignment=CrossAxisAlignment.CENTER,
+                    spacing=26
+                )
             )
-        )
+        page.update()
+
+    def view_pop(e: ViewPopEvent) -> None:
+        page.views.pop()
+        top_view: View = page.views[-1]
+        page.go(top_view.route)
+
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+    page.go(page.route)
+
+
+if __name__ == "__main__":
+    ft.app(target=main)
