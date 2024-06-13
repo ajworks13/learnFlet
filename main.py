52914a13590f2bcd6269d757d5534b7c5b8bc794
Aneudy Consuegra <ilovethe80s90s@yahoo.com>
2024-06-12 23:58:31 -0400

    playing with navigation through tut

--- main.py
+++ main.py
@@ -1,63 +1,25 @@
 import flet as ft
+from flet import View, Page, AppBar, ElevatedButton, Text
+from flet import RouteChangeEvent, ViewPopEvent, CrossAxisAlignment, MainAxisAlignment
 
 
-def main(page: ft.Page):
-    page.title = "Containers - clickable and not"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+def main(page: Page) -> None:
+    page.title = "My Store"
 
-    page.add(
-        ft.Row(
-            [
-                ft.Container(
-                    content=ft.Text("Non clickable"),
-                    margin=10,
-                    padding=10,
-                    alignment=ft.alignment.center,
-                    bgcolor=ft.colors.BLUE_100,
-                    width=150,
-                    height=150,
-                    border_radius=10,
-                ),
-                ft.Container(
-                    content=ft.Text("Clickable without Ink"),
-                    margin=10,
-                    padding=10,
-                    alignment=ft.alignment.center,
-                    bgcolor=ft.colors.GREEN_200,
-                    width=150,
-                    height=150,
-                    border_radius=10,
-                    on_click=lambda e: print("Clickable without Ink clicked!"),
-                ),
-                ft.Container(
-                    content=ft.Text("Clickable with Ink"),
-                    margin=10,
-                    padding=10,
-                    alignment=ft.alignment.center,
-                    bgcolor=ft.colors.CYAN_200,
-                    width=150,
-                    height=150,
-                    border_radius=10,
-                    ink=True,
-                    on_click=lambda e: print("Clickable with Ink clicked!"),
-                ),
-                ft.Container(
-                    content=ft.Text("Clickable transparent with Ink"),
-                    margin=10,
-                    padding=10,
-                    alignment=ft.alignment.center,
-                    width=150,
-                    height=150,
-                    border_radius=10,
-                    ink=True,
-                    on_click=lambda e: print(
-                        "Clickable transparent with Ink clicked!"),
-                ),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        ),
-    )
+    def route_change(e: RouteChangeEvent) -> None:
+        page.views.clear()
 
-
-ft.app(target=main)
+        page.views.append(
+            View(
+                route='/',
+                controls=[
+                    AppBar(title=Text("Home"), bgcolor='blue'),
+                    Text(value='Home', size=30),
+                    ElevatedButton(text='Go to store',
+                                   on_click=lambda _: page.go('/store'))
+                ],
+                vertical_alignment=MainAxisAlignment.CENTER,
+                horizontal_alignment=CrossAxisAlignment.CENTER,
+                spacing=26
+            )
+        )
