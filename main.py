1a02a27a347b05ae23640fc078719db80d3c4d9c
Aneudy Consuegra <ilovethe80s90s@yahoo.com>
2024-06-15 11:21:19 -0400

    learning more on buttons

--- main.py
+++ main.py
@@ -3,21 +3,29 @@ import flet as ft
 
 def main(page: ft.page):
 
+    # functions need to be inside def main
+
+    def addTask(p):
+        checkBox = ft.Checkbox(value=False)
+        checkBoxText = ft.Text(value=textField.value,
+                               width=350, bgcolor="WHITE", size=20)
+
+        taskRow = ft.Row(controls=[
+            checkBox,
+            checkBoxText
+        ],
+            alignment=ft.MainAxisAlignment.START
+        )
+
+        page.add(taskRow)
+
     page.window_width = 500
     page.window_height = 700
-    page.bgcolor = "#E5B8F4"
+    page.bgcolor = "WHITE"
+
     textField = ft.TextField(width=350)
-    addBtn = ft.ElevatedButton(text="Add")
+    addBtn = ft.FloatingActionButton(icon=ft.icons.ADD, on_click=addTask)
 
-    checkBox = ft.Checkbox(value=True)
-    checkBoxText = ft.Text(value="Task 1", width=350, bgcolor="WHITE", size=20)
-
-    taskRow = ft.Row(controls=[
-        checkBox,
-        checkBoxText
-    ],
-        alignment=ft.MainAxisAlignment.START
-    )
     entiresRow = ft.Row(controls=[
         textField,
         addBtn
@@ -25,7 +33,7 @@ def main(page: ft.page):
         alignment=ft.MainAxisAlignment.SPACE_BETWEEN
     )
 
-    page.add(entiresRow, taskRow)
+    page.add(entiresRow)
 
 
 ft.app(target=main)
