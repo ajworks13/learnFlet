77b4447fb6d5c624e5e9b89dbd8275c82fc98fc2
Aneudy Consuegra <ilovethe80s90s@yahoo.com>
2024-06-15 11:05:32 -0400

    doing another flet tut

--- main.py
+++ main.py
@@ -1,57 +1,75 @@
 import flet as ft
-from flet import View, Page, AppBar, ElevatedButton, Text
-from flet import RouteChangeEvent, ViewPopEvent, CrossAxisAlignment, MainAxisAlignment
 
 
-def main(page: Page) -> None:
-    page.title = "My Store"
+def main(page: ft.page):
 
-    def route_change(e: RouteChangeEvent) -> None:
-        page.views.clear()
+    page.window_width = 500
+    page.window_height = 700
+    page.bgcolor = "#E5B8F4"
+    textField = ft.TextField(width=350)
+    addBtn = ft.ElevatedButton(text="Add")
+    entiresRow = ft.Row(controls=[
+        textField,
+        addBtn
+    ],
+        alignment=ft.MainAxisAlignment.SPACE_BETWEEN
+    )
 
-        # Home
-        page.views.append(
-            View(
-                route='/',
-                controls=[
-                    AppBar(title=Text("Home"), bgcolor='blue'),
-                    Text(value='Home', size=30),
-                    ElevatedButton(text='Go to store',
-                                   on_click=lambda _: page.go('/store'))
-                ],
-                vertical_alignment=MainAxisAlignment.CENTER,
-                horizontal_alignment=CrossAxisAlignment.CENTER,
-                spacing=26
-            )
-        )
-
-        # Store
-        if page.route == '/store':
-            page.views.append(
-                View(
-                    route='/store',
-                    controls=[
-                        AppBar(title=Text('Store'), bgcolor='blue'),
-                        Text(value='Store', size=30),
-                        ElevatedButton(
-                            text='Go back', on_click=lambda _: page.go('/'))
-                    ],
-                    vertical_alignment=MainAxisAlignment.CENTER,
-                    horizontal_alignment=CrossAxisAlignment.CENTER,
-                    spacing=26
-                )
-            )
-        page.update()
-
-    def view_pop(e: ViewPopEvent) -> None:
-        page.views.pop()
-        top_view: View = page.views[-1]
-        page.go(top_view.route)
-
-    page.on_route_change = route_change
-    page.on_view_pop = view_pop
-    page.go(page.route)
+    page.add(entiresRow)
 
 
-if __name__ == "__main__":
-    ft.app(target=main)
+ft.app(target=main)
+
+
+# def main(page: Page) -> None:
+#     page.title = "My Store"
+
+#     def route_change(e: RouteChangeEvent) -> None:
+#         page.views.clear()
+
+#         # Home
+#         page.views.append(
+#             View(
+#                 route='/',
+#                 controls=[
+#                     AppBar(title=Text("Home"), bgcolor='blue'),
+#                     Text(value='Home', size=30),
+#                     ElevatedButton(text='Go to store',
+#                                    on_click=lambda _: page.go('/store'))
+#                 ],
+#                 vertical_alignment=MainAxisAlignment.CENTER,
+#                 horizontal_alignment=CrossAxisAlignment.CENTER,
+#                 spacing=26
+#             )
+#         )
+
+#         # Store
+#         if page.route == '/store':
+#             page.views.append(
+#                 View(
+#                     route='/store',
+#                     controls=[
+#                         AppBar(title=Text('Store'), bgcolor='blue'),
+#                         Text(value='Store', size=30),
+#                         ElevatedButton(
+#                             text='Go back', on_click=lambda _: page.go('/'))
+#                     ],
+#                     vertical_alignment=MainAxisAlignment.CENTER,
+#                     horizontal_alignment=CrossAxisAlignment.CENTER,
+#                     spacing=26
+#                 )
+#             )
+#         page.update()
+
+#     def view_pop(e: ViewPopEvent) -> None:
+#         page.views.pop()
+#         top_view: View = page.views[-1]
+#         page.go(top_view.route)
+
+#     page.on_route_change = route_change
+#     page.on_view_pop = view_pop
+#     page.go(page.route)
+
+
+# if __name__ == "__main__":
+#     ft.app(target=main)
