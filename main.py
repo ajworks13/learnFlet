47c896334065ebfed190fbc764f1c47c00e05c62
Aneudy Consuegra <ilovethe80s90s@yahoo.com>
2024-06-16 10:16:05 -0400

    utilizing oop

--- main.py
+++ main.py
@@ -1,42 +1,71 @@
 import flet as ft
 
 
+class TaskApp(ft.UserControl):
+
+    def build(self):
+        self.textField = ft.TextField(width=350)
+        self.addBtn = ft.FloatingActionButton(
+            icon=ft.icon.ADD, on_click=self.addClick)
+
+        self.task = ft.Column
+        taskRow = ft.Column(controls=[
+            ft.Row(controls=[self.textField, self.addBtn])
+        ])
+
+    def addClick(self, e):
+        pass
+
+    def taskDelete(self, task):
+        pass
+
+
 def main(page: ft.page):
-
-    # functions need to be inside def main
-
-    def addTask(p):
-        checkBox = ft.Checkbox(value=False)
-        checkBoxText = ft.Text(value=textField.value,
-                               width=350, bgcolor="WHITE", size=20)
-
-        taskRow = ft.Row(controls=[
-            checkBox,
-            checkBoxText
-        ],
-            alignment=ft.MainAxisAlignment.START
-        )
-
-        page.add(taskRow)
-
+    page.title = "Tasking app by aj"
     page.window_width = 500
     page.window_height = 700
     page.bgcolor = "WHITE"
 
-    textField = ft.TextField(width=350)
-    addBtn = ft.FloatingActionButton(icon=ft.icons.ADD, on_click=addTask)
 
-    entiresRow = ft.Row(controls=[
-        textField,
-        addBtn
-    ],
-        alignment=ft.MainAxisAlignment.SPACE_BETWEEN
-    )
-
-    page.add(entiresRow)
+ft.app(tartget=main)
 
 
-ft.app(target=main)
+# def main(page: ft.page):
+
+#     # functions need to be inside def main
+
+#     def addTask(p):
+#         checkBox = ft.Checkbox(value=False)
+#         checkBoxText = ft.Text(value=textField.value,
+#                                width=350, bgcolor="WHITE", size=20)
+
+#         taskRow = ft.Row(controls=[
+#             checkBox,
+#             checkBoxText
+#         ],
+#             alignment=ft.MainAxisAlignment.START
+#         )
+
+#         page.add(taskRow)
+
+#     page.window_width = 500
+#     page.window_height = 700
+#     page.bgcolor = "WHITE"
+
+#     textField = ft.TextField(width=350)
+#     addBtn = ft.FloatingActionButton(icon=ft.icons.ADD, on_click=addTask)
+
+#     entiresRow = ft.Row(controls=[
+#         textField,
+#         addBtn
+#     ],
+#         alignment=ft.MainAxisAlignment.SPACE_BETWEEN
+#     )
+
+#     page.add(entiresRow)
+
+
+# ft.app(target=main)
 
 
 # def main(page: Page) -> None:
